dec24bc37e9619cc467fa9604381b388d9ab86a8
Jun Luo <luojunmoo@gmail.com>
2025-01-26 11:49:29 -0800

    update for toggleterm and format

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,10 +1,9 @@
-vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
-vim.g.mapleader = " "
 vim.cmd("nnoremap <C-H> <C-W>h")
 vim.cmd("nnoremap <C-J> <C-W>j")
 vim.cmd("nnoremap <C-K> <C-W>k")
 vim.cmd("nnoremap <C-L> <C-W>l")
+
